e099705dae99f06f44b5ea5f181daccbe8fa8536
osama <osamagamil2070@gmail.com>
2022-09-19 07:09:37 +0400

    body1

--- lib/screens/Programbody.dart
+++ lib/screens/Programbody.dart
@@ -15,11 +15,33 @@ class Programbody extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(bottom: 20),
               child: Container(
+                margin: EdgeInsets.only(top: 80),
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(40),
                         topRight: Radius.circular(40))),
+              ),
+            ),
+            Container(
+              height: 190,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 25,
+                              offset: Offset(0, 15),
+                              color: Colors.black12)
+                        ]),
+                    height: 166,
+                  )
+                ],
               ),
             )
           ],
